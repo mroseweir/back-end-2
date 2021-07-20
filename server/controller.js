8dc3ad92d399c161058172cbf209b0bba0d354e3
mroseweir <roseweirmichael@gmail.com>
2021-07-20 13:18:10 -0500

    test

--- server/controller.js
+++ server/controller.js
@@ -29,7 +29,7 @@ module.exports = {
         houses.push(newHouse)
         houseId++
         res.status(200).send(houses)
-        Rollbar.info('house added succesfully')
+        rollbar.info('house added succesfully')
     },
     updateHouse: (req, res) => {
         const { id } = req.params;
