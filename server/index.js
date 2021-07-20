32df725590ba80fde0fda4231cc2b160632351fe
mroseweir <roseweirmichael@gmail.com>
2021-07-20 12:14:35 -0500

    added heroku lines

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const control = require('./controller');
-const port = 4004
+// const port = 4004
 
 //middlewars
 app.use(express.json());
@@ -14,5 +14,6 @@ app.post('/api/houses', control.createHouse);
 app.put('/api/houses/:id', control.updateHouse);
 app.delete('/api/houses/:id', control.deleteHouse);
 
+const port = process.env.PORT || 4500
 
 app.listen(port, () => {console.log(`Server is up and running on ${port}`)})
