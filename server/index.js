3d75f999a9a703327d5fac0230df039476a1550a
mroseweir <roseweirmichael@gmail.com>
2021-07-20 13:06:17 -0500

    adds rollbar

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const app = express();
 const cors = require('cors');
 const control = require('./controller');
 // const port = 4004
+const Rollbar = require("rollbar")
+
+const rollbar = new Rollbar({
+    accessToken: 'e6362af4dd944f49b6dff5d54164ab3e',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 app.use(express.static("public"));
 const path = require("path");
